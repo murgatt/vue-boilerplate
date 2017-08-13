fbd0fca652309b9c27bc45b54c93a9f6c1e796b3
murgatt <theomurgat@hotmail.fr>
2017-08-13 14:08:26 +0200

    Added: less and example page vue-router

---
+++ src/pages/ExamplePage.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="ExamplePage">
+		This is a simple example page for vue-router
+	</div>
+</template>
+
+<script>
+    export default {
+        name: 'example-page',
+		components: {
+            
+        },
+	    data() {
+            return {
+                
+            }
+	    },
+	    methods: {
+            
+	    },
+    }
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+	.ExamplePage {
+
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from '@/components/Hello';
+import ExamplePage from '@/pages/ExamplePage';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+    routes: [
+        {
+            path:      '/',
+            name:      'Hello',
+            component: Hello
+        },
+        {
+            path: '/examplePage',
+            name: 'ExamplePage',
+            component: ExamplePage
+        },
+    ]
+});

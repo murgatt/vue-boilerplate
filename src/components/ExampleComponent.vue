95f0f7fc46e6c37ef2ec166412a55fe31a49e2f5
murgatt <theomurgat@hotmail.fr>
2017-09-11 19:10:33 +0200

    Added: example component

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="ExampleComponent">
+		And this is an example component
+	</div>
+</template>
+
+<script>
+    export default {
+        name: 'example-component',
+		components: {
+            
+        },
+	    data() {
+            return {
+                
+            }
+	    },
+	    methods: {
+            
+	    },
+    }
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+	.ExampleComponent {
+		width: 200px;
+		margin: 12px auto;
+		border: 1px solid #42b983;
+	}
+</style>
--- src/pages/ExamplePage.vue
+++ src/pages/ExamplePage.vue
@@ -1,14 +1,18 @@
 <template>
 	<div class="ExamplePage">
-		This is a simple example page for vue-router
+		This is a simple and awesome empty example page
+		<router-link to="/">Return to index page</router-link>
+		<example-component></example-component>
 	</div>
 </template>
 
 <script>
+	import ExampleComponent from '@/components/ExampleComponent';
+
     export default {
         name: 'example-page',
 		components: {
-            
+            ExampleComponent
         },
 	    data() {
             return {
